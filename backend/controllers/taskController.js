ce4fd009d07a98187d66803e0c9fe0e81021619f
satakshirawat25 <satakshirawat.25may@gmail.com>
2026-01-12 15:58:54 +0530

    task controller done

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -304,6 +304,25 @@ const taskPriorityLevels = taskPriorities.reduce((acc, priority) => {
        },{})
 
        //fetch recent 10 task
+
+       const recentTasks = await Task.find()
+       .sort({createdAt:-1})
+       .limit(10)
+       .select("title status priority dueDate createdAt")
+
+       res.status(200).json({
+        statistics:{
+            totalTasks,
+            pendingTasks,
+            completedTasks,
+            overdueTasks
+        },
+        charts:{
+            taskDistribution,
+            taskPriorityLevels,
+        },
+        recentTasks,
+       })
     } catch (error) {
         res.status(500).json({
             message:"Server error",
@@ -314,6 +333,66 @@ const taskPriorityLevels = taskPriorities.reduce((acc, priority) => {
 
 export const getUserDashboardData = async(req,res)=>{
     try {
+        const userId = req.user._id //only fetch data for loggein user
+
+            const totalTasks = await Task.countDocuments({assignedTo:userId})
+            const pendingTasks = await Task.countDocuments({assignedTo:userId,status:"Pending"})
+            const completedTasks = await Task.countDocuments({assignedTo:userId,status:"Cmpleted"})
+            const overdueTasks = await Task.countDocuments({
+                assignedTo:userId,
+                status:{$ne:"Completed"},
+            dueDate:{$lt:new Date()}
+        
+            
+            })
+
+             const taskStatuses = ["Pending","In Progress","Completed"]
+       const taskDistributionRaw = await Task.aggregate([
+        {$match:{assignedTo:userId}},
+        {$group:{_id:"$status",count:{$sum:1}}},
+       ])
+
+       //task distribution by status
+       const taskDistribution = taskStatuses.reduce((acc,status)=>{
+         const formattedKey = status.replace(/\s+/g, ""); // Remove spaces for response keys
+  acc[formattedKey] =
+    taskDistributionRaw.find((item) => item._id === status)?.count || 0;
+  return acc;
+}, {});
+taskDistribution["All"] = totalTasks; 
+
+const taskPriorities = ["Low", "Medium", "High"];
+const taskPriorityLevelsRaw = await Task.aggregate([
+   {$match:{assignedTo:userId}},
+        {$group:{_id:"$priority",count:{$sum:1}}},
+       ])
+
+const taskPriorityLevels = taskPriorities.reduce((acc, priority) => {
+  acc[priority] =
+    taskPriorityLevelsRaw.find((item) => item._id === priority)?.count || 0;
+  return acc;
+       },{})
+
+       //fetch task
+       const recentTasks = await Task.find({assignedTo:userId})
+       .sort({createdAt:-1})
+       .limit(10)
+       .select("title status priority dueDate createdAt")
+
+       res.status(200).json({
+        statistics:{
+            totalTasks,
+            pendingTasks,
+            completedTasks,
+            overdueTasks
+        },
+        charts:{
+            taskDistribution,
+            taskPriorityLevels,
+        },
+        recentTasks,
+       })
+
         
     } catch (error) {
         res.status(500).json({
